f0bf9955d42d5f98654c95a7568c49acd94cc8d4
first_name <ss329222@gmail.com>
2022-02-15 08:05:03 +0530

    Added:PrimeNumbers

---
+++ src/com/blz/practice/PrimeNumber.java
@@ -0,0 +1,27 @@
+package com.blz.practice;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		int num, i, count = 0;
+		Scanner s = new Scanner(System.in);
+
+		System.out.print("Enter a Number: ");
+		num = s.nextInt();
+		for (i = 1; i < num; i++) {
+
+			if (num % i == 0) {
+				count++;
+				break;
+			}
+		}
+
+		if (count == 2)
+			System.out.println("\nIt is a Prime Number.");
+		else
+			System.out.println("\nIt is not a Prime Number.");
+	}
+
+}
